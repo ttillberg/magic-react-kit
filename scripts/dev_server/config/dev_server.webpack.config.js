69ab6c936319f0ec445db9f1180bcdb6a03b4904
Theo Tillberg <ttillberg@gmail.com>
2018-03-07 20:51:04 +0100

    Remove uglify on server

--- scripts/dev_server/config/dev_server.webpack.config.js
+++ scripts/dev_server/config/dev_server.webpack.config.js
@@ -39,7 +39,7 @@ module.exports = {
     new webpack.NamedModulesPlugin(),
     new webpack.HotModuleReplacementPlugin(),
     new webpack.NoEmitOnErrorsPlugin(),
-    new webpack.optimize.UglifyJsPlugin(),
+
     new webpack.DefinePlugin({
       'process.env': {
         NODE_ENV: JSON.stringify('development'),
