865830e11a966e7e31dfe0dfabad9d291bcc8a85
Theo Tillberg <ttillberg@gmail.com>
2018-03-16 12:22:29 +0100

    Discard custom warning

--- lib/hot_entry/HotApp.jsx
+++ lib/hot_entry/HotApp.jsx
@@ -1,13 +1,6 @@
 import React from 'react'
 import { hot } from 'react-hot-loader'
 
-try {
-  var App = require('__APP_ENTRY__').default
-} catch (e) {
-  console.log(
-    "Couldn't locate main App entry point, make sure the --path points to the source folder"
-  )
-  process.exit(1)
-}
+var App = require('__APP_ENTRY__').default
 
 export default hot(module)(App)
